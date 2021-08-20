fa568b34af1582fb6eea81410405befe163c40db
youngjun0313 <zerojun0313@kaist.ac.kr>
2021-08-20 17:55:54 +0900

    5. complete

--- 5.longest_palindromic_substring.py
+++ 5.longest_palindromic_substring.py
@@ -1,17 +1,34 @@
+# 오래 걸림.
+from os import PathLike
+
+
 def longestPalindrome(s: str) -> str:
     if(s == ""):
-        return 0
-    if(len(s) == 1):
-        return 1
-    maxLen = 0
-    temp = ""
-    ans = ""
-    for i in range(len(s)):
-        temp = temp + s[i]
-        subTemp = temp[temp.index(s[i]) : ]
-        revSubTemp = ''.join(reversed(subTemp))
-        if(temp.index(s[i]) != len(temp)-1 and subTemp == revSubTemp):
-            if(len(subTemp) > maxLen):
-                maxLen = len(subTemp)
-                ans = subTemp
-    return ans
+        return s
+    palinArray = []
+    palinStart = 0
+    palinEnd = 0
+    for k in range(len(s)):
+        palinArray.append([])
+        for i in range(len(s)-k):
+            palinArray[k].append(False)
+
+    for k in range(len(s)):
+        for i in range(len(s)-k):
+            j = i + k
+            if(k==0):
+                palinArray[i][j] = True
+                palinStart = i
+                palinEnd = j
+            elif(k == 1 or k == 2):
+                if(s[i] == s[j]):
+                    palinArray[i][j] = True
+                    palinStart = i
+                    palinEnd = j
+            else:
+                if(palinArray[i+1][j-1] and s[i] == s[j]):
+                    palinArray[i][j] = True
+                    palinStart = i
+                    palinEnd = j
+
+    return s[palinStart: palinEnd+1]
