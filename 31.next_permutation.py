7791d14807507bdaef022c515792775a0d2932f2
youngjun0313 <zerojun0313@kaist.ac.kr>
2021-08-23 14:03:24 +0900

    31. complete: 7

--- 31.next_permutation.py
+++ 31.next_permutation.py
@@ -1,19 +1,34 @@
+# 오래 걸림.
 from typing import *
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         check = False
         index = None
         n = len(nums)
-        for i in range(1, n):
-            if(nums[i-1] < nums[i]):
+        for i in range(n-1):
+            if(nums[n-i-2]< nums[n-i-1]):
                 check = True
-                index = i
-        if(check and index != None):
-            temp = nums[index]
-            nums[index] = nums[index-1]
-            nums[index-1] = temp
-        else:
-            for i in range(n//2 - 1):
+                min = nums[n-i-1]
+                index = n-i-1
+                for j in range(n-i, n):
+                    if(nums[j]<min and nums[j]>nums[n-i-2]):
+                        min = nums[j]
+                        index = j
+                nums[index] = nums[n-i-2]
+                nums[n-i-2] = min
+                for j in range(index, n-1):
+                    if(nums[j] < nums[j+1]):
+                        temp = nums[j]
+                        nums[j] = nums[j+1]
+                        nums[j+1] = temp
+                for j in range((i-1)//2+1):
+                    temp = nums[n-i-1+j]
+                    nums[n-i-1+j] = nums[n-1-j]
+                    nums[n-j-1] = temp
+                break
+        
+        if(not check):
+            for i in range(n//2):
                 temp = nums[n-1-i]
-                nums[i] = nums[n-1-i]
-                nums[n-1-i] = temp
+                nums[n-1-i] = nums[i]
+                nums[i] = temp
