ddbf5083fee36adadbaccddd8f2c3b81ac78fdd8
youngjun0313 <zerojun0313@kaist.ac.kr>
2022-05-20 22:59:02 +0900

    ignore test

--- sandbox.py
+++ sandbox.py
@@ -1,31 +1,3 @@
-from collections import deque
-def solution(n, edge):
-    adjs=[]
-    for _ in range(n):
-        adjs.append([])
-    for e in edge:
-        adjs[e[0]-1].append(e[1]-1)
-        adjs[e[1]-1].append(e[0]-1)
-    visited=[False]*n
-    distances=[0]*n
-    Q=deque()
-    Q.append(0)
-    md=0
-    ans=0
-    while(len(Q)>0):
-        node = Q.popleft()
-        if(not visited[node]):
-            visited[node]=True
-            for i in adjs[node]:
-                print(i)
-                if(not visited[i] and distances[i]==0):
-                    Q.append(i)
-                    distances[i]=distances[node]+1
-                    if(md<distances[i]):
-                        ans=1
-                        md=distances[i]
-                    elif(md==distances[i]):
-                        ans+=1
-    return ans
-        
-print(solution(6, [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]))
+dic[b]
+
+print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["*rodo", "*rodo", "******"]))
