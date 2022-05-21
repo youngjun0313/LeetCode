58ee79ee1e0ad397dd7c3dff86b7fa70004a2d86
youngjun0313 <zerojun0313@kaist.ac.kr>
2022-05-21 17:16:45 +0900

    0003 complete - speed: 96.67%, memory: 77.80%

--- 0003.median_of_two_sorted_arrays.java
+++ 0003.median_of_two_sorted_arrays.java
@@ -1,20 +1,23 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int l = nums1.length + nums2.length;
-        int[] nums = new int[l];
+        int n1=nums1.length;
+        int n2=nums2.length;
+        int l = n1 + n2;
+        int[] nums;
         int pn=0;
         int pm=0;
 
-        if(nums1.length==0) {
+        if(n1==0) {
             nums = nums2;
-        } else if(nums2.length==0) {
+        } else if(n2==0) {
             nums = nums1;
         } else {
-            for(int i=0; i<l; i++) {
-                if(pn>=nums1.length) {
+            nums = new int[l/2+1];
+            for(int i=0; i<l/2+1; i++) {
+                if(pn>=n1) {
                     nums[i] = nums2[pm];
                     pm++;
-                } else if(pm>=nums2.length) {
+                } else if(pm>=n2) {
                     nums[i] = nums1[pn];
                     pn++;
                 } else {
