c729d79db40bc054732ed535f4ac8d4900f8cd81
youngjun0313 <zerojun0313@kaist.ac.kr>
2022-05-21 14:05:00 +0900

    0075 complete low speed

--- 0075.sort_colors.java
+++ 0075.sort_colors.java
@@ -1,5 +1,26 @@
 class Solution {
     public void sortColors(int[] nums) {
+        List<Integer> zeros = new ArrayList<>();
+        List<Integer> ones = new ArrayList<>();
+        List<Integer> twos = new ArrayList<>();
+        List<Integer> numbers = new ArrayList<>();
 
+        for(int e: nums) {
+            if(e==0) {
+                zeros.add(e);
+            } else if (e==1) {
+                ones.add(e);
+            } else {
+                twos.add(e);
+            }
+        }
+
+        numbers.addAll(zeros);
+        numbers.addAll(ones);
+        numbers.addAll(twos);
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = numbers.get(i);
+        }
     }
 }
