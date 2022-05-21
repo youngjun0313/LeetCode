9b3b4e006b33c1b7dea66b01e7f905243f56d854
youngjun0313 <zerojun0313@kaist.ac.kr>
2022-05-21 16:37:52 +0900

    0075 complete speed: 100%, memory: 64.94%

--- 0075.sort_colors.java
+++ 0075.sort_colors.java
@@ -1,26 +1,24 @@
 class Solution {
     public void sortColors(int[] nums) {
-        List<Integer> zeros = new ArrayList<>();
-        List<Integer> ones = new ArrayList<>();
-        List<Integer> twos = new ArrayList<>();
-        List<Integer> numbers = new ArrayList<>();
+        int one_pivot = 0;
+        int two_pivot = 0;
 
-        for(int e: nums) {
-            if(e==0) {
-                zeros.add(e);
-            } else if (e==1) {
-                ones.add(e);
-            } else {
-                twos.add(e);
+        for(int i = 0; i< nums.length; i++) {
+            if(nums[i] == 0) {
+                swap(nums, i, two_pivot);
+                swap(nums, two_pivot, one_pivot);
+                one_pivot++;
+                two_pivot++;
+            } else if(nums[i] == 1) {
+                swap(nums, i, two_pivot);
+                two_pivot++;
             }
         }
+    }
 
-        numbers.addAll(zeros);
-        numbers.addAll(ones);
-        numbers.addAll(twos);
-
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = numbers.get(i);
-        }
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
